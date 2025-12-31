06eebb24edecae3217bef82e8a7e74437fde05cc
Mark wu <markwu@Marks-MacBook-Air.local>
2025-12-31 12:18:24 -0500

    fibonacci number

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int fibonacci(int n)
+{ 
+    if (n == 0) { return 0;}
+    int memory_size = (n+1)*4;
+    int *arr = (int *) malloc(memory_size);
+    
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i=2; i <= n; ++i)
+    {
+         arr[i] = arr[i-1] + arr [i-2];
+    }
+    return arr[n];
+}
+
 int main(int argc, char* argv[])
 {
     printf("Which fibonacci number do you want to know?\n");
@@ -9,9 +24,20 @@ int main(int argc, char* argv[])
     scanf("%d", &n);
     
     printf("Your input is: %d\n", n);
-
-    printf("I will calculate the %d-th Fibonacci number for you!\n",n);
-
+    if (n < 0)
+    {
+        printf("Please input a positive intergers/n");
+        return 1;
+    }
+    int f = fibonacci(n);
+    if (n == 1)
+    {
+        printf("The first Fibonacci number is: %d\n",f);
+    }
+    else
+    {
+        printf("The %d-th Fibonacci number is: %d\n",n,f);
+    }
     return 0;
 }
 
