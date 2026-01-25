9c328b5f62d9a0bc8454dccd660b6987c6fa66a0
Mark wu <wumark45@gmail.com>
2026-01-25 12:12:06 -0500

    Tokenize a comma seperate input

---
+++ inserction.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    /*arg 1: comma seperated sorted numbers like 1,3,5,7,9
+     *      argv[1][] = "1,3,5,17,29"
+     *arg 2: the number to be inserted 
+     * output: a sorted array
+     */
+
+    if (argc < 3)
+    {
+        printf("Please input a comma-seperated array and a number!\n");
+        return 1;
+    }
+
+    int n = strlen(argv[1]);
+    int comma_count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (argv[1][i] == ',')
+        {
+        comma_count = comma_count + 1;
+        }
+    }
+   
+    //the total # of integers from input 1
+    int length = comma_count + 1;
+
+    // allocate memory for the array of integers
+    int *a = (int *) malloc(length * 4);
+    // printf("%d\n", comma_count);
+  
+    a[0] = atoi(&argv[1][0]);
+
+    int j = 1;
+
+    for (int i = 0; i < n; ++i)
+    {  
+        if (argv[1][i] ==',')
+        {
+             a[j] = atoi(&argv[1][i+1]);
+             j += 1; 
+        }
+    }
+  
+    for (int i=0; i<length; ++i)
+    {
+        printf("%d\n", a[i]);
+    }
+ 
+    free(a); a = NULL;
+    return 0;
+}
