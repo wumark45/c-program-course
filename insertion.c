dffe470d64c997f5145165738f9e1d5e74836853
Mark wu <wumark45@gmail.com>
2026-01-31 12:00:52 -0500

    insert a interger into a sorted array

--- insertion.c
+++ insertion.c
@@ -2,6 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 
+int array_insert(int *b, int len, int y)
+{
+    /* insert y into b */
+    int j=0;
+    for(int i=0; i<len; ++i)
+    {
+        if(b[i]>y)
+        {   
+            j=i;
+            break;
+        }
+    }
+    printf("j=%d\n", j);
+    
+    for( int i=len-2; i>=j; --i)
+    {
+        b[i+1] =b[i];
+    }
+    
+    b[j]=y;
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
     /*arg 1: comma seperated sorted numbers like 1,3,5,7,9
@@ -30,7 +54,7 @@ int main(int argc, char *argv[])
     int length = comma_count + 1;
 
     // allocate memory for the array of integers
-    int *a = (int *) malloc(length * 4);
+    int *a = (int *) malloc((length+1) * 4);
     // printf("%d\n", comma_count);
   
     a[0] = atoi(&argv[1][0]);
@@ -46,11 +70,22 @@ int main(int argc, char *argv[])
         }
     }
   
-    for (int i=0; i<length; ++i)
+    for (int i=0; i<length+1; ++i)
     {
-        printf("%d\n", a[i]);
+        printf("%d ", a[i]);
     }
+    printf("\n");
+
+    int x = atoi(argv[2]);
+    array_insert(a, length+1,  x);
  
+    for (int i=0; i<length+1; ++i)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+
     free(a); a = NULL;
     return 0;
 }
