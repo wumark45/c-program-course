6058d9a51a52dab2ebf4e2980fe35d07d36b3e99
Mark wu <wumark45@gmail.com>
2026-02-07 12:05:58 -0500

    finding min

--- sort.c
+++ sort.c
@@ -2,6 +2,24 @@
 #include<stdlib.h>
 #include<string.h>
 
+int min_value(int *a, int length)
+{
+    int q=0;
+    int minimum=a[0];
+    for (int i=0; i<length ;++i)
+    {
+        printf("%d\n",a[i]);
+        if (minimum>a[i])
+        {
+            q=i;
+            minimum=a[i];
+        }  
+    }
+    printf("Location %d\n",q);
+    printf("Value %d\n", minimum);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     int n = strlen(argv[1]);
@@ -16,7 +34,7 @@ int main(int argc, char *argv[])
     int length = comma_count + 1;
     
     int *a = (int *) malloc((length+1) * 4);
-    
+    a[0]=atoi(&argv[1][0]); 
     int j = 1;
 
     for (int i=0; i<n; ++i)
@@ -28,5 +46,6 @@ int main(int argc, char *argv[])
         }
     }
     printf("%d\n",j);
+    min_value(a,length);
     return 0;
 }
