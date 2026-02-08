d0f531c63941e03c788f514de5408f1adb8d8bcf
Mark wu <wumark45@gmail.com>
2026-02-08 12:04:50 -0500

    sort interger values

--- sort.c
+++ sort.c
@@ -2,26 +2,47 @@
 #include<stdlib.h>
 #include<string.h>
 
+int print_array(int *a, int len)
+{
+    for (int i=0; i<len; ++i)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
 int min_value(int *a, int length)
 {
     int q=0;
     int minimum=a[0];
     for (int i=0; i<length ;++i)
     {
-        printf("%d\n",a[i]);
+       // printf("%d\n",a[i]);
         if (minimum>a[i])
         {
             q=i;
             minimum=a[i];
         }  
     }
-    printf("Location %d\n",q);
-    printf("Value %d\n", minimum);
+   // printf("Location %d\n",q);
+    //printf("Value %d\n", minimum);
+   
+    a[q]=a[0];
+    a[0]=minimum;   
+
+   // print_array(a, length);
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        printf("Please input a comma-seperated array and a number!\n");
+        return 1;
+    }
+
     int n = strlen(argv[1]);
     int comma_count = 0;
     for (int i=0; i<n; ++i)
@@ -33,7 +54,7 @@ int main(int argc, char *argv[])
     }
     int length = comma_count + 1;
     
-    int *a = (int *) malloc((length+1) * 4);
+    int *a = (int *) malloc((length) * 4);
     a[0]=atoi(&argv[1][0]); 
     int j = 1;
 
@@ -45,7 +66,12 @@ int main(int argc, char *argv[])
             j += 1;
         }
     }
-    printf("%d\n",j);
-    min_value(a,length);
+    print_array(a, length);
+  //printf("%d\n",j);
+    for(int i=0; i<length-1; ++i)
+    {
+        min_value(a+i,length-i);
+    }   
+    print_array(a,length);
     return 0;
 }
