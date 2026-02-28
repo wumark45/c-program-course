3041ce7f9f60b2b758834666e216138afad7beaa
wumark45 <wumark45@gmail.com>
2026-02-28 12:15:47 -0500

    use my toolbox myarray

--- sort_array.c
+++ sort_array.c
@@ -1,39 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "myarray.h"
 
-int print_array(int *a, int len)
-{
-    for (int i=0; i<len; ++i)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    return 0;
-}
-
-int min_value(int *a, int length)
-{
-    int q=0;
-    int minimum=a[0];
-    for (int i=0; i<length ;++i)
-    {
-       // printf("%d\n",a[i]);
-        if (minimum>a[i])
-        {
-            q=i;
-            minimum=a[i];
-        }  
-    }
-   // printf("Location %d\n",q);
-    //printf("Value %d\n", minimum);
-   
-    a[q]=a[0];
-    a[0]=minimum;   
-
-   // print_array(a, length);
-    return 0;
-}
 
 int main(int argc, char *argv[])
 {
@@ -66,12 +35,9 @@ int main(int argc, char *argv[])
             j += 1;
         }
     }
+    
     print_array(a, length);
-  //printf("%d\n",j);
-    for(int i=0; i<length-1; ++i)
-    {
-        min_value(a+i,length-i);
-    }   
+    sort_array(a, length);
     print_array(a,length);
     return 0;
 }
