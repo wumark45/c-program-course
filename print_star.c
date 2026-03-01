65d0c59b0498a33e48a4f30127ef1e803ca7bac4
wumark45 <wumark45@gmail.com>
2026-03-01 11:48:01 -0500

    seperate for loop

--- print_star.c
+++ print_star.c
@@ -1,23 +1,35 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+int print_space(int space)
+{   
+    for (int x=0; x<space; ++x)
+        {
+            printf(" ");
+        }
+    return 0;
+}
+ 
+int print_char(int number, char c)
+{
+    for(int j=0; j<number; ++j)
+        {              
+            printf("%c", c);
+        }
+    return 0;
+}
+
 int print_star(int row,char character)
 {
     for(int i=0; i<row; ++i)
     {
-        int number = i*2+1;
-        int space = row-i;
-        for (int x=0; x<space; ++x)
-        {
-            printf(" ");
-        }
-        for(int j=0; j<number; ++j)
-        {              
-            printf("%c", character);
-        }
+        int n = i*2+1;
+        int s = row-i;
+        print_space(s);
+        print_char(n, character);
         printf("\n");
-    }
-    return 0;
+     }
+     return 0;
 }
 
 int main(int argc, char *argv[])
