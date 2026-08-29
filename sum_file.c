f979a3afedfc78ff796cb3c4c4b462aac9190646
wumark45 <wumark45@gmail.com>
2026-08-29 11:27:30 -0400

    refactored

--- sum_file.c
+++ sum_file.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+int tok_sum(char *buf)
+{
+    int sum = 0;
+    char delims[] = ",";
+    char *token = strtok(buf, delims);
+    while (token != NULL)
+    {
+        sum = atoi(token) + sum;
+        token = strtok(NULL, delims);
+    }
+    return sum;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -16,24 +29,16 @@ int main(int argc, char *argv[])
         printf("Cannot open file.\n");
         return 2;
     }
+    int count = 0;
     char buf[1000];
     char *p = fgets(buf, sizeof(buf), fp);
     while (p != NULL)
     {
-        char delims[] = ",";
-        char *token = strtok(buf, delims);
-        int count = 0;
-        int sum = 0;
-        while (token != NULL)
-        {
-            count ++;
-            sum = atoi(token)+sum;
-            token = strtok(NULL, delims);
-        }
-        printf("count %d,sum: %d\n", count, sum);
+        int sum = tok_sum(buf);
+        printf("count: %d,sum: %d\n",count,sum);
+        count++;
         p = fgets(buf, sizeof(buf), fp);
     }
     fclose(fp);
     return 0;
 }
-
